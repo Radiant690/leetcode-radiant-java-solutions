426f183a521fe103146d24bfd1a65c5ffab4b20e
kadamati venkatasai <venkatasaikadamati@gmail.com>
2022-10-03 23:52:34 +0530

    Update optimal_solution.java

--- 88. Merge Sorted Array/optimal_solution.java
+++ 88. Merge Sorted Array/optimal_solution.java
@@ -7,10 +7,12 @@
 // @lc code=start
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // ? We come up with insertion sort approach taking advantage of the sorted arrays.
         int runner1 = m - 1;
         int runner2 = n - 1;
         int resultRunner = m + n - 1;
 
+        // !  Consider each element `nums1[i]` of array `nums1` and ignore the element if it is already in the correct order; otherwise, swap it with the next smaller element, which happens to be the first element of `nums2`.
         while (runner1 >= 0 && runner2 >= 0) {
             if (nums1[runner1] > nums2[runner2]) {
                 nums1[resultRunner] = nums1[runner1];
