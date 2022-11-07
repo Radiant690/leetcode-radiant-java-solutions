4900d234636758b3a8a59414f92d540e1158bf0d
kadamati venkatasai <venkatasaikadamati@gmail.com>
2022-11-07 19:07:47 +0530

    added number to char approach

---
+++ .history/1323. Maximum 69 Number/string_builder_approach_20221107185925.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1323 lang=java
+ *
+ * [1323] Maximum 69 Number
+ */
+
+// @lc code=start
+class Solution {
+    public int maximum69Number (int num) {
+        // ? Intuition :: What if the num was a character array, with ease of access of specific index. We can convert the given num into an char array and iterate over it. We now simply look for the conditional whether the particular index value is 
+    }
+}
+// @lc code=end
+
---
+++ .history/1323. Maximum 69 Number/string_builder_approach_20221107190241.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1323 lang=java
+ *
+ * [1323] Maximum 69 Number
+ */
+
+// @lc code=start
+class Solution {
+    public int maximum69Number (int num) {
+        // ? Intuition :: What if the num was a character array, with ease of access of specific index. We can convert the given num into an char array and iterate over it. We now simply look for the conditional whether the particular index value is 6 then assign it to 9 and break the logic there.
+
+        char num_arr[] = String.valueOf(num).toCharArray();
+
+        for(int i = 0; i< num_arr)
+    }
+}
+// @lc code=end
+
